a3dd3be6c75c080a206fe76b5f2cbb2163434433
Henry Aviles <haviles04@gmail.com>
2023-05-21 10:22:42 -0400

    feat: state logic

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,16 @@
 import "@/styles/globals.css";
 import ThemeContextProvider from "@/context/ThemeContextProvider";
+import InvoiceContextProvider from "@/context/InvoiceContextProvider";
 import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeContextProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeContextProvider>
+    <InvoiceContextProvider>
+      <ThemeContextProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeContextProvider>
+    </InvoiceContextProvider>
   );
 }
