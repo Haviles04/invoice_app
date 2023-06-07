41b13cf168fd35a2b475fdff5fce6a1118692386
Henry Aviles <haviles04@gmail.com>
2023-06-07 18:17:17 -0400

    feat: added darkmode and CSS, destructured invoice

--- src/components/InvoiceBar.js
+++ src/components/InvoiceBar.js
@@ -1,15 +1,25 @@
 import React from "react";
+import Image from "next/image";
 import styles from "@/styles/InvoiceBar.module.css";
+import ArrowRight from "@/assets/icon-arrow-right.svg";
+import { useTheme } from "@/context/ThemeContextProvider";
 
 function InvoiceBar({ invoice }) {
-  const dueDate = new Date(invoice.paymentDue).toDateString().slice(3);
+  const { theme } = useTheme();
+  const { id, paymentDue, clientName, total, status } = invoice;
+
+  const dueDate = new Date(paymentDue).toDateString().slice(3);
+
   return (
-    <div className={styles.container}>
-      <p className="heading-S-variant">#{invoice.id}</p>
-      <p className="body-font">Due {dueDate}</p>
-      <p className="body-font">{invoice.clientName}</p>
-      <p className="heading-S-variant">${invoice.total.toFixed(2)}</p>
-      <p>{invoice.status}</p>
+    <div className={`${styles.container} ${styles[theme]}`}>
+      <p className="heading-S-variant">#{id}</p>
+      <p className={`body-font ${styles.lightText}`}>Due {dueDate}</p>
+      <p className={`body-font ${styles.lightText}`}>{clientName}</p>
+      <p className={`heading-S-variant ${styles.total}`}>${total.toFixed(2)}</p>
+      <p className={`${styles[status]} heading-S-variant `}>{status}</p>
+      <button className={styles.arrowBtn}>
+        <Image src={ArrowRight} alt="Arrow" />
+      </button>
     </div>
   );
 }
